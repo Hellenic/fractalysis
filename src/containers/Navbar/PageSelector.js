410ca06980240fdbb718808968788d1301977de0
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2017-10-15 08:29:57 +0300

    Fixed page selector functionality

--- src/containers/Navbar/PageSelector.js
+++ src/containers/Navbar/PageSelector.js
@@ -4,16 +4,30 @@ import { Dropdown } from 'semantic-ui-react';
 import TextIcon from '../../components/TextIcon/TextIcon';
 import routes from '../../routes';
 
-const options = [
-  { key: 0, value: 0, content: <TextIcon icon="eye" title="Editor" to="/" /> },
-  { key: 1, value: 1, content: <TextIcon icon="world" title="Scenes" to="/scenes" /> },
-  { key: 2, value: 2, content: <TextIcon icon="image" title="Renders" to="/renders" /> }
+const links = [
+  { icon: 'eye', title: 'Editor', to: '/' },
+  { icon: 'world', title: 'Scenes', to: '/scenes' },
+  { icon: 'image', title: 'Renders', to: '/renders' }
 ];
 
-const PageSelector = ({ location }) => {
+const PageSelector = ({ location, history }) => {
   const currentIndex = routes.findIndex(r => r.path === location.pathname);
+  const link = links[currentIndex];
   return (
-    <Dropdown labeled trigger={options[currentIndex].content} icon="dropdown" options={options} />
+    <Dropdown labeled trigger={<TextIcon icon={link.icon} title={link.title} />} icon="dropdown">
+      <Dropdown.Menu>
+        {
+          links.map(link => (
+            <Dropdown.Item
+              key={`link-${link.to}`}
+              icon={link.icon}
+              text={link.title}
+              onClick={() => history.push(link.to)}
+            />
+          ))
+        }
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
