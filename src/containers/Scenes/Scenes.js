92b53f0e1779b9a45806e59a0898d1a2064fb568
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2018-02-13 20:28:22 +0200

    Refactor some of configurations to new presets

--- src/containers/Scenes/Scenes.js
+++ src/containers/Scenes/Scenes.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { stringify } from 'qs';
+import { Link } from 'react-router-dom';
+import presets from '../../presets.json';
 
 class Scenes extends Component {
   render() {
     return (
       <section>
-        <h1>Scenes will be here</h1>
+        <h1>Preset scenes</h1>
+        <ul>
+          {
+            presets.map((p, index) => {
+              const params = stringify(Object.assign({}, { shader: p.shader, ...p.uniforms }));
+              return (
+                <li>
+                  <Link key={index} to={`/?${params}`}>
+                    {p.name}
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
       </section>
     );
   }
