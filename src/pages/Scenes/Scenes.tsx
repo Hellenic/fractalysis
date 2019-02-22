e4c4b7f71fea2741e1907ddcff7ee5e9d8de9610
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2019-02-22 10:19:17 +0200

    Scenes will now display user-made scenes

--- src/pages/Scenes/Scenes.tsx
+++ src/pages/Scenes/Scenes.tsx
@@ -3,12 +3,34 @@ import { Link } from 'react-router-dom';
 import { Card, Container } from 'semantic-ui-react';
 import { stringify } from 'qs';
 import Fractal from '../../components/Fractal/Fractal';
+import storage from '../../utils/storage';
 import presets from '../../presets.json';
 
 class Scenes extends Component {
   render() {
+    const scenes = storage.getScenes();
+
     return (
       <Container>
+        <h1>User scenes</h1>
+        <Card.Group>
+          {scenes.map((scene, index) => {
+            const params = stringify(
+              Object.assign({}, { shader: scene.shader, ...scene.uniforms })
+            );
+            return (
+              <Link key={index} to={`/?${params}`}>
+                <Card>
+                  <Fractal width={200} height={150} quality={1} {...scene} />
+                  <Card.Content>
+                    <Card.Header>{scene.name}</Card.Header>
+                    <Card.Description>Shader: {scene.shader}</Card.Description>
+                  </Card.Content>
+                </Card>
+              </Link>
+            );
+          })}
+        </Card.Group>
         <h1>Preset scenes</h1>
         <Card.Group>
           {presets.map((p, index) => {
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,5 @@
 import { Uniforms, Scene } from '../types/';
+import Scenes from '../pages/Scenes/Scenes';
 
 export function storeScene(
   presetName: string,
@@ -7,12 +8,18 @@ export function storeScene(
 ): void {
   const scene = { name: presetName, shader: shaderName, uniforms } as Scene;
 
-  const presets: Scene[] = JSON.parse(localStorage.getItem('presets') || '[]');
-  presets.push(scene);
+  const scenes: Scene[] = getScenes();
+  scenes.push(scene);
 
-  localStorage.setItem('presets', JSON.stringify(presets));
+  localStorage.setItem('presets', JSON.stringify(scenes));
+}
+
+export function getScenes(): Scene[] {
+  const scenes: Scene[] = JSON.parse(localStorage.getItem('presets') || '[]');
+  return scenes;
 }
 
 export default {
-  storeScene
+  storeScene,
+  getScenes
 };
