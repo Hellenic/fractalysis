7d5f6a5a5cdf6d2223db80192a00b33a2d8d6c51
Hannu Kärkkäinen <hannu.karkkainen@reaktor.com>
2019-02-22 07:35:16 +0200

    Initial SaveButton logic

--- src/pages/Editor/components/SaveButton/SaveButton.tsx
+++ src/pages/Editor/components/SaveButton/SaveButton.tsx
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
+import { Confirm, Modal, Input, InputOnChangeData } from 'semantic-ui-react';
 import TextIcon from '../../../../components/TextIcon/TextIcon';
 import withUniforms from '../../../../hocs/withUniforms';
 import { Uniforms, WithUniformsProps } from '../../../../types/index';
 
-class SaveButton extends Component<WithUniformsProps> {
-  handleClick = () => {
+interface IState {
+  dialogOpen: boolean;
+}
+
+class SaveButton extends Component<WithUniformsProps, IState> {
+  state = {
+    dialogOpen: false
+  };
+
+  handleConfirm = () => {
     const { uniforms, shader } = this.props;
     console.log('TODO Save this fractal to a user preset', shader, uniforms);
+
+    this.setState({ dialogOpen: false });
+  };
+
+  handleInput = (event: React.SyntheticEvent, data: InputOnChangeData) => {
+    const { value } = data;
+    console.log('Data', value);
   };
 
   render() {
+    const { dialogOpen } = this.state;
     return (
-      <TextIcon
-        icon="save"
-        size="big"
-        title="Save"
-        onClick={this.handleClick}
-      />
+      <React.Fragment>
+        <Confirm
+          open={dialogOpen}
+          header="Save scene as a preset"
+          content={
+            <Modal.Content>
+              <Modal.Description>
+                <p>Give a name for the preset</p>
+                <Input placeholder="Preset name" onChange={this.handleInput} />
+              </Modal.Description>
+            </Modal.Content>
+          }
+          onCancel={() => this.setState({ dialogOpen: false })}
+          onConfirm={this.handleConfirm}
+        />
+        <TextIcon
+          icon="save"
+          size="big"
+          title="Save"
+          onClick={() => this.setState({ dialogOpen: true })}
+        />
+      </React.Fragment>
     );
   }
 }
