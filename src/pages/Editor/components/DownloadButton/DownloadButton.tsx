d048c1d96339003bd844c537ee516c0d41da1f19
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2019-02-22 12:36:07 +0200

    Clean up DownloadButton

---
+++ src/pages/Editor/components/DownloadButton/DownloadButton.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { stringify } from 'qs';
+import TextIcon from '../../../../components/TextIcon/TextIcon';
+import withUniforms from '../../../../hocs/withUniforms';
+import { WithUniformsProps } from '../../../../types/index';
+
+class DownloadButton extends Component<WithUniformsProps> {
+  onDownload = () => {
+    const { uniforms, shader } = this.props;
+
+    const queryString = stringify(
+      Object.assign({}, { shader }, uniforms, { download: true })
+    );
+
+    window.open(`${window.location.origin}/?${queryString}`, '_blank');
+  };
+
+  render() {
+    return (
+      <TextIcon
+        icon="download"
+        size="big"
+        title="Download"
+        onClick={this.onDownload}
+      />
+    );
+  }
+}
+
+export default withUniforms(DownloadButton);
