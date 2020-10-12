a6b07c0764b0b392eed630662de4ceaf3df7d4bf
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2020-10-12 10:58:27 +0300

    Add some margin to the loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './Loader.css';
 
-const Loader = props => <div className="lds-dual-ring" />;
+const Loader = props => (
+  <div className="loader">
+    <div className="lds-dual-ring" />
+  </div>
+);
 
 Loader.propTypes = {
   visible: PropTypes.bool
